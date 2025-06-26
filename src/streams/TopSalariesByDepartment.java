26903746d5638800df67290e028881db3105383a
PavanN38 <pavannayini38@gmail.com>
2025-06-26 17:02:44 -0500

    added the emplyer object

---
+++ src/streams/TopSalariesByDepartment.java
@@ -0,0 +1,37 @@
+package streams;
+
+public class TopSalariesByDepartment {
+    public static void main(String[] args) {}
+    
+   
+}
+
+class Employee {
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + salary;
+    }
+}
+
